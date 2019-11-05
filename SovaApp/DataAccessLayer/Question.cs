df55b854cacbb308de64dacc4ff07663ff4bb566
gari-marcos <gari_marcos@hotmail.com>
2019-11-05 12:15:40 +0100

    Question toString method

--- SovaApp/DataAccessLayer/Question.cs
+++ SovaApp/DataAccessLayer/Question.cs
@@ -19,5 +19,11 @@ namespace DataAccessLayer
         public String Title { get; set; }
         
         public int UserId { get; set; }
+
+        public override string ToString()
+        {
+            return "Id:" + Id + " Accepted Answer Id:" + AcceptedAnswerId + " Creation Date:" + CreationDate + " Score:" + Score + " Body:" + Body
+                + " Closed Date:" + ClosedDate + " Title:" + Title + " User Id:" + UserId;
+        }
     }
 }
