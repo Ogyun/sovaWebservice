69aa64c6d56f76947f271c991872825befafc221
gari-marcos <gari_marcos@hotmail.com>
2019-11-05 12:18:39 +0100

    ListQuestions method

--- SovaApp/DataAccessLayer/SearchService.cs
+++ SovaApp/DataAccessLayer/SearchService.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccessLayer
@@ -33,6 +34,32 @@ namespace DataAccessLayer
             return searchResultList;
         }
 
+        public List<SearchResult> SearchByAcceptedAnswer(Boolean accepted)
+        {
+            using var db = new  SovaDbContext();
+            if(accepted) { var result = db.Questions.FromSqlRaw("select * from questions where acceptedanswerid is not null"); }
+            else{ var result = db.Questions.FromSqlRaw("select * from questions where acceptedanswerid is null"); }
+            return ListQuestions(result);
+        }
+
+        private List<SearchResult> ListResults(IQueryable<SearchResult> result)
+        {
+            var searchResultList = new List<SearchResult>();
+            SearchResult searchResult;
+            foreach (var item in result)
+            {
+                searchResult = new SearchResult
+                {
+                    PostId = item.PostId,
+                    Type = item.Type,
+                    Rank = item.Rank,
+                    Body = item.Body
+                };
+                searchResultList.Add(searchResult);
+            }
+            return searchResultList;
+        }
+
         public Question GetQuestionById(int questionId)
         {
             using var db = new SovaDbContext();
@@ -43,6 +70,27 @@ namespace DataAccessLayer
         {
             using var db = new SovaDbContext();
             return db.Answers.Find(answerId);
+        }
+        
+        private List<Question> ListQuestions(IQueryable<Question> result)
+        {
+            var searchResultList = new List<Question>();
+            foreach (var item in result)
+            {
+                var searchResult = new Question
+                {
+                    Id = item.Id,
+                    AcceptedAnswerId = item.AcceptedAnswerId,
+                    CreationDate = item.CreationDate,
+                    Score = item.Score,
+                    Body = item.Body,
+                    ClosedDate = item.ClosedDate,
+                    Title = item.Title,
+                    UserId = item.UserId
+                };
+                searchResultList.Add(searchResult);
+            }
+            return searchResultList;
         }
     }
 }
