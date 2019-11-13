ce6d3a57fe5337cdad59ba903758840927810716
Ogyun <ogyun95@gmail.com>
2019-11-13 19:51:12 +0100

    getAllMarkedQuestionByUserEmail copied to IQuestionService

--- SovaApp/DataAccessLayer/Contracts/IQuestionService.cs
+++ SovaApp/DataAccessLayer/Contracts/IQuestionService.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace DataAccessLayer.Repositories
 {
     public interface IQuestionService
     {
         Question GetQuestionById(int questionId);
+        public List<Question> GetAllMarkedQuestionsByUserEmail(string userEmail);
     }
 }
