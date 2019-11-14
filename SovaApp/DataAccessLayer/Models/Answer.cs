eabcd4a0a44db0981b4cf5ec33df207d6dd3bc87
Harsimran Singh <harsisi@ruc.dk>
2019-11-14 12:26:45 +0100

    answerservice test

--- SovaApp/DataAccessLayer/Models/Answer.cs
+++ SovaApp/DataAccessLayer/Models/Answer.cs
@@ -14,6 +14,6 @@ namespace DataAccessLayer
         public string Body { get; set; }
         // public int UserId { get; set; }
        // public User User { get; set; }
-       // public Question Question { get; set; }
+       public Question Question { get; set; }
     }
 }
