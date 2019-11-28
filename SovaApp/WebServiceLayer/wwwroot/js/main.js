656fb325e41daaa63304d568301a7f5611d4145b
Ogyun <ogyun95@gmail.com>
2019-11-28 13:45:55 +0100

    frontend skeleton

---
+++ SovaApp/WebServiceLayer/wwwroot/js/main.js
@@ -0,0 +1,14 @@
+﻿require.config({
+    baseUrl: "js",
+    paths: {
+        knockout: "../lib/knockout/build/output/knockout-latest.debug",
+        text: "../lib/requirejs-text/text"
+
+    }
+});
+
+require(["knockout", "app"], function (ko, app, ds) {
+    //console.log(app.name);
+
+    ko.applyBindings(app);
+});
