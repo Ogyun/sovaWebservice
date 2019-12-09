e720704ad918212c05bc2d940aea8e5b11049ce4
IvanNS <ivan_ns3@abv.bg>
2019-12-09 20:22:47 +0100

    Added the retype password field to make sure the user has entered the correct password.

--- SovaApp/WebServiceLayer/wwwroot/js/components/signup-component/signup-component.js
+++ SovaApp/WebServiceLayer/wwwroot/js/components/signup-component/signup-component.js
@@ -3,16 +3,20 @@
         var email = ko.observable("");
         var password = ko.observable("");
         var name = ko.observable("");
+        var rpassword = ko.observable("");
         var signupSubmit = function () {
-            su.signup(email, password, name, function (response) {
-                console.log(response)
-            });
-
+            if (password() === rpassword()) {
+                su.signup(email, password, name, function (response) {
+                    console.log(response)
+                });
+            }
+            else alert("Passwords do not match!");
         }
         return {
             email,
             password,
             name,
+            rpassword,
             signupSubmit
         }
     }
