23614fba6306d39a3928518cedae31edc0eae428
Ogyun <ogyun95@gmail.com>
2019-12-09 14:04:20 +0100

    searchBy:keyword, score,tag,acceptedAnswer implemented

--- SovaApp/WebServiceLayer/wwwroot/js/components/search-component/search-component.js
+++ SovaApp/WebServiceLayer/wwwroot/js/components/search-component/search-component.js
@@ -4,10 +4,45 @@
 
     var searchClick = function () {
         var query = document.getElementById("searchInput").value;
-        ss.searchByKeyword(query, function (response) {
-            posts(response);
-            console.log(response[0])
-        });
+
+        if (query.startsWith("tag:")) {
+            var tags = query.substr(4);
+            console.log(tags);
+            ss.searchByTag(tags, function (response) {
+                posts(response);
+            });
+        }
+
+        else if (query.startsWith("score:")) {
+            var score = query.substr(6);
+            ss.searchByScore(score, function (response) {
+                posts(response);
+            });
+        }
+
+        else if (query.startsWith("accepted:")) {
+            var accepted = query.substr(9, 3);
+            var keywords = "";
+
+            if (accepted.includes("yes")) {
+                accepted = accepted.slice(0, 3);
+                keywords = query.substr(13)
+            }
+            if (accepted.includes("no")) {
+                accepted = accepted.slice(0, 2);
+                keywords = query.substr(12)
+            }
+            ss.searchByAcceptedAnswer(accepted, keywords, function (response) {
+                posts(response);
+            });
+        }
+
+        else {
+            ss.searchByKeyword(query, function (response) {
+                posts(response);
+                console.log(response[0])
+            });
+        }
     }
 
     return {
--- SovaApp/WebServiceLayer/wwwroot/js/services/searchService.js
+++ SovaApp/WebServiceLayer/wwwroot/js/services/searchService.js
@@ -1,8 +1,7 @@
 ﻿define([], function () {
 
-    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im9neXVuQGdtYWlsLmNvbSIsIm5iZiI6MTU3NTg0MTc1OSwiZXhwIjoxNTc1ODUyNTU5LCJpYXQiOjE1NzU4NDE3NTl9.pdGW61RwdPp3fUNjHcxMjhza41jfu2IJOLIjTqHcySQ";
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im9neXVuQGdtYWlsLmNvbSIsIm5iZiI6MTU3NTg5NjUyMiwiZXhwIjoxNTc1OTA3MzIyLCJpYXQiOjE1NzU4OTY1MjJ9._NO9PX2yiGc4G7SCySETFKHZ0OjxyEDuTwY6lt3Q5zo";
     var headers = new Headers();
-    //headers.append('Content-Type', 'application/json');
     headers.append('Authorization','Bearer ' + token);
 
 
@@ -15,7 +14,67 @@
         callback(data);
     }
 
+    var searchByScore = async function (query, callback) {
+        var response = await fetch("http://localhost:5001/api/search/score/" + query, {
+            method: 'GET',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
+    var searchByTag = async function (query, callback) {
+        var response = await fetch("http://localhost:5001/api/search/tags/" + query, {
+            method: 'GET',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
+
+    var searchByAcceptedAnswer = async function (accepted,keywords, callback) {
+        var response = await fetch("http://localhost:5001/api/search/answer/" + accepted + "/" + keywords,{
+            method: 'GET',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
+
+    var getSearchHistory = async function (email, callback) {
+        var response = await fetch("http://localhost:5001/api/search/history/" + email, {
+            method: 'GET',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
+
+    var deleteSearchHistoryById = async function (historyId, callback) {
+        var response = await fetch("http://localhost:5001/api/search/history/" + historyId, {
+            method: 'DELETE',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
+
+    var deleteAllSearchHistory = async function (email, callback) {
+        var response = await fetch("http://localhost:5001/api/search/history/user" + email, {
+            method: 'DELETE',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
+
+
     return {
-        searchByKeyword
+        searchByKeyword,
+        searchByScore,
+        searchByTag,
+        searchByAcceptedAnswer,
+        getSearchHistory,
+        deleteSearchHistoryById,
+        deleteAllSearchHistory       
     };
 });
