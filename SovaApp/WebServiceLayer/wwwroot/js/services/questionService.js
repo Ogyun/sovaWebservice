178867858f1d01f2941ed8eac4fb37b306603104
Ogyun <ogyun95@gmail.com>
2019-12-11 15:53:32 +0100

    questionService functons implemented

--- SovaApp/WebServiceLayer/wwwroot/js/services/questionService.js
+++ SovaApp/WebServiceLayer/wwwroot/js/services/questionService.js
@@ -1,8 +1,32 @@
 ﻿define([], function () {
-    //question related functions that make api calls
+
+    var token = localStorage.getItem('token');
+    var headers = new Headers();
+    headers.append('Authorization', 'Bearer ' + token);
+
+
+    var getSpecificQuestion = async function (questionId, callback) {
+        var response = await fetch("http://localhost:5001/api/questions/" + questionId, {
+            method: 'GET',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
+
+    var getQuestionAnswers =  = async function (questionId, callback) {
+        var response = await fetch("http://localhost:5001/api/" + questionId +"/answers", {
+            method: 'GET',
+            headers: headers
+        });
+        var data = await response.json();
+        callback(data);
+    }
 
     return {
-        //return functions
+        getSpecificQuestion,
+        getQuestionAnswers
+
 
 
     };
