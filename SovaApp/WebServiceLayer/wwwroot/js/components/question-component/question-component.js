0193a08b8da0b754e2f0205ab472f8f1cb6ccce1
Ogyun <ogyun95@gmail.com>
2019-12-12 14:16:02 +0100

    question component displays question body

--- SovaApp/WebServiceLayer/wwwroot/js/components/question-component/question-component.js
+++ SovaApp/WebServiceLayer/wwwroot/js/components/question-component/question-component.js
@@ -1,7 +1,18 @@
-﻿define(['knockout'], function (ko) {
-    return function () {
-        return {
+﻿define(['knockout', 'store'], function (ko, store) {
 
+    var question = ko.observable(store.getState().selectedQuestion);
+
+    return function () {
+     
+        console.log(question().body);
+
+        store.subscribe(function () {
+            var state = store.getState();
+            question(state.selectedQuestion);
+        });
+
+        return {
+            question
         };
     };
 });
